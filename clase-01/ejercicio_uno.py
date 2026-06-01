6bafc7255e015bbea450e652ebdd4289dfb038e8
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-01 10:25:54 -0300

    Concatenación y casteo. Enunciado Ej.1

---
+++ clase-01/ejercicio_uno.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 1: Variables y tipos (8 min + 15 min)
+
+Consigna: Crear un programa que guarde en variables:
+
+* El nombre
+* El apellido
+* La altura en metros (números decimales)
+* Si sos estudiante o no (verdadero o falso)
+Mostrar en pantalla todos los datos
+"""
+
+print("Ejecicio Uno")
--- clase-01/main.py
+++ clase-01/main.py
@@ -56,3 +56,26 @@ print(nombre_completo)
 # join (muchas cadenas)
 nombre_completo = " ".join([nombre, segundo_nombre, apellido])
 print(nombre_completo)
+
+texto = "La" * 3
+print(texto) # LaLaLa
+
+print("Función input(): Para pedir datos al usuario")
+
+# print(input("Escriba aquí su nombre: "))
+dato = input("Escriba aquí su nombre: ")
+print(dato)
+print(f"Hola {dato}")
+print("Hola " + dato)
+
+print("Casteo (Conversión de tipos)")
+
+cantidad_alumnos = input("¿Cuántos alumnos están en clase? ")
+print(cantidad_alumnos) # Esto es una cadena
+# print(cantidad_alumnos + 2) No puedo hacer operaciones aritmeticas con cadenas
+print(type(cantidad_alumnos)) # str
+cantidad_alumnos = int(cantidad_alumnos) # casteo una cadena a número
+print(type(cantidad_alumnos)) # int
+cantidad_personas = cantidad_alumnos + 2
+print(cantidad_personas)
+print(type(cantidad_personas)) # int
