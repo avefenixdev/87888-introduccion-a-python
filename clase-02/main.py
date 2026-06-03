8a42ca4e266c66ab228f369579808b2983f8c875
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-03 11:19:38 -0300

    listas y matrices

--- clase-02/main.py
+++ clase-02/main.py
@@ -55,3 +55,98 @@ match dia_semana:
         print(f"{dia_semana} es un día fin de semana")
     case _:
         print(f"{dia_semana} no es un día de la semana válido")
+
+print('--------------------------------')
+
+# ! Listas (Mutables)
+## Las listas nos permite almacenar varios valores. Son indexadas (Arreglo... array... vector )
+## - Y que se puedan modificar
+## - Agregar nuevos elementos
+## - Reordenar
+## (alumnos, producto de carrito)
+## Lista alumnos, lista de los productos dentro del carrito.
+
+""" 
+nombre = "Pedro"
+nombre2 = "Carlos"
+nombre3 = "Ana"
+ """
+
+nombres = ["Pedro", "Carlos", "Ana"]
+
+## Listas homogeneas (Recomendables) -> Mismo tipo de dato
+## Lista de números
+numeros = [ 10, 20, 30, 40 ]
+## Lista de cadenas
+##            0          1         2
+frutas = ["manzana", "banana", "naranja"]
+
+## Listas heterogeneas ()
+## Lista mixta
+##        0     1      2      3
+datos = [ 25, "Maxi", 1.70, True ]
+
+## Lista vacía
+lista_vacia = []
+print(lista_vacia)
+# Quiero imprimir la cadena 'manzana' por la consola'
+#             0          1         2
+frutas = ["manzana", "banana", "naranja"]
+
+print(frutas[0]) # manzana
+print(frutas[2]) # naraja
+
+
+frutas[1] = "sandia"
+print(frutas[1]) # sandia
+print(frutas)
+print(len(frutas))
+print("Último elemento de la lista")
+print(frutas[-1])
+print(frutas[len(frutas)-1])
+
+print("------------------------------------")
+
+animales = ["tigre", "buho", "gato", "caballo", "vizcacha", "serpiente", "perro"]
+
+print(animales)
+print(len(animales))
+
+animales[4] = 'tortuga'
+print(animales)
+
+print("Recorro la lista")
+#   0               7
+for i in range(len(animales)): 
+    print(i, animales[i])
+
+# Matrices
+#            0       1
+matriz = [[1, 2], [3, 4]]
+#          0  1    0  1
+
+print(matriz)
+print("# Accedo al valor '3'")
+print(matriz[1]) # [3, 4]
+print(matriz[1][0]) # 3
+print("# Modifico el valor 2 por 22")
+
+matriz[0][1] = 22
+print(matriz)
+
+# matriz = [[1, 2], [3, 4]]
+print("-------------------------")
+# matriz = 
+#   ⬇️ ⬇️ <---- columnas
+#  [[1, 22], <--- fila
+#   [3, 4]] <--- fila
+
+# Recorriendo filas
+for fila in matriz: 
+    print(fila)
+
+print("-------------------------")
+# Recorriendo filas y valores de esas filas (columnas)
+for fila in matriz:
+    for valor in fila: 
+        print(valor)
