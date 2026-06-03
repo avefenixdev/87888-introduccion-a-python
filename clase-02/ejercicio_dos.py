adfaf2b44ca9071c9fdc30b7c14df67a720ac4ff
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-03 11:26:58 -0300

    consigna ejercicio 3

--- clase-02/ejercicio_dos.py
+++ clase-02/ejercicio_dos.py
@@ -7,6 +7,6 @@ El programa debe:
 2. Convertir a la moneda indicada (dolores o euros) [hits: condicionales (if, match)]
 3. Mostrar cuántos dólares o euros puede comprar. [hits: concantenado]
     Ejemplo de lo que debería verse en pantalla:
-        3.1 Se vería: "Con 15000 pesso se comprar 100 dolares"
-        3.2 Se vería: "Con 15000 pesso se comprar 100 euros"
+        3.1 Se vería: "Con 15000 pesos se comprar 100 dolares"
+        3.2 Se vería: "Con 15000 pesos se comprar 100 euros"
 """
---
+++ clase-02/ejercicio_tres.py
@@ -0,0 +1,12 @@
+# Ejercicio: Listas
+## Consigna: Crear una lista de números. Y luego:
+
+# - [10, 22, 55, 2, 88, 7]
+
+# - Imprimir cada número 
+# - Calcular la suma de todos -> 10+22+55+2+88+7
+# - Calcular el promedio -> (10+22+55+2+88+7)/cantidad-elementos
+# - Mostrar la suma de los número y el promedio
+# - El formato que necesito es el siguiente
+#   - La suma de los números dentro de la lista es: suma
+#   - El promedio de los números dentro de la lista es: promedio
