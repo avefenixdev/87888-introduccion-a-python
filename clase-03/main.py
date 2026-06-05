6f4c8cefbb9aaee38b304fd8db447e4c98648eb8
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-05 11:35:22 -0300

    Ejercicios resuelto en clase

--- clase-03/main.py
+++ clase-03/main.py
@@ -61,6 +61,8 @@ print(f"El total de la lista uno es: {resultado_uno}")
 print("----------------------------------")
 resultado_dos = sumar_lista(lista_dos)
 print(f"El total de la lista dos es: {resultado_dos}" )
+print("-" * 50)
+print("-" * 50)
 
 """ 
 Enunciado:
@@ -73,7 +75,6 @@ Se pide:
     - Calcular el gasto promedio 
     - Mostrar ambos valores por pantalla
  """
-
 # MVC
 gastos = [1200, 650, 850, 450] # Modelos
 
@@ -85,6 +86,9 @@ promedio = total / cant_elementos
 # Vista
 print("Gasto total:", total)
 print("Gasto promedio:", promedio)
+
+print("-" * 50)
+print("-" * 50)
 
 """ 
 Enunciado:
@@ -99,16 +103,114 @@ Se debe:
     - Mostrar ambos valores por pantalla
 """
 
-gastos = [1200, 650, 850, 450]
+gastos = []
 cant_elementos = len(gastos)
-val_minimo = min(gastos)
+# val_minimo = min(gastos)
 
 if cant_elementos == 0:
     print("No hay gastos cargados")
-elif val_minimo < 0:
+elif min(gastos) < 0:
     print("Error: hay gastos negativos.")
 else: 
     total = sum(gastos)
     promedio = total / cant_elementos
     print("Gasto total:", total)
     print("Gastos promedio:", promedio)
+
+print('-' * 50)
+print('-' * 50)
+
+""" 
+El usuario debe ingresar gastos uno por uno
+El ingreso termina cuando escribe 0
+
+El programa debe:
+    - Repetir el pedido mientras el gasto sea válido
+    - No aceptar números negativos
+    - Calcular total y promedio
+"""
+
+""" print('Inicio')
+while True:
+    gasto = float(input("Ingreso un gasto (0 para terminar):"))
+
+    if gasto == 0:
+        print('Entró al break')
+        break # me saca del ciclo
+
+    if gasto == 200:
+        print('Entró al continue')
+        continue # me saca de la iteración actual
+
+    print('Siguiente iteración')
+
+print('Fin') """
+
+""" gastos = []
+
+while True:
+    gasto = float(input("Ingreso un gasto (0 para terminar):"))
+
+    if gasto == 0:
+        break # me saca del ciclo
+    elif gasto < 0:
+        print('Error: El gasto no puede ser negativo.')
+    else:
+        gastos.append(gasto) # | -> def agregar_gasto_lista(gasto): void
+    
+    if len(gastos) == 0:
+        print('No se cargaron gastos.')
+    else:
+        # | -> def cantidad_elementos(gastos): cant_elementos
+        total = sum(gastos) # | -> def calcular_total_gastos(gastos): total
+        promedio = total / len(gastos) # | def calcular_promedio_gastos(total, cant_elementos): promedio
+        print("Gasto total:", total) # | def mostrar_total: void
+        print("Gasto promedio:", promedio) # def mostrar_promedio: void """
+
+print("*" * 50)
+print("*" * 50)
+
+gastos = []
+total = 0
+promedio = 0
+
+def agregar_gasto_lista(gasto):
+    gastos.append(gasto)
+
+def cantidad_elementos(gastos): 
+    cant_elementos = len(gastos)
+    return cant_elementos
+    
+def calcular_total_gastos(gastos): 
+    total = sum(gastos)
+    return total
+
+def calcular_promedio_gastos(total, cant_elementos):
+    promedio = total / cant_elementos
+    return promedio
+
+def mostrar_total(total): 
+    print("Gasto total:", total)
+
+def mostrar_promedio(promedio):
+    print("Gasto promedio:", promedio)
+
+while True:
+    gasto = float(input("Ingreso un gasto (0 para terminar):"))
+
+    if gasto == 0:
+        break # me saca del ciclo
+    elif gasto < 0:
+        print('Error: El gasto no puede ser negativo.')
+    else:
+        agregar_gasto_lista(gasto) 
+    
+    if cantidad_elementos(gastos) == 0:
+        print('No se cargaron gastos.')
+    else:
+        total = calcular_total_gastos(gastos) 
+        cant_elementos = cantidad_elementos(gastos)
+        promedio = calcular_promedio_gastos(total, cant_elementos)
+        
+mostrar_total(total)
+mostrar_promedio(promedio)
