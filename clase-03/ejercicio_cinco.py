2c160eb06a4f1ceed4eea8e9fd98abc772bf14ac
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-05 11:40:11 -0300

    Enunciado ejercicio 5

---
+++ clase-03/ejercicio_cinco.py
@@ -0,0 +1,10 @@
+# Ejercicio Cinco
+# El usuario ingresa notas del alumno
+# El ingreso termina cuando el usuario ingresar un -1
+
+# El programa debe:
+#   - Repetir el pedido mientras la nota sea válida
+#   - Acetar solo valores entre 1 y 10
+#   - Mostrar las notas (5 - 2 - 7 - 10)
+#   - Calcular promedio e indicar si aprueba o no
+#       - Aprueba con 4 o más. (Aprobaste o Recuperatorio)
